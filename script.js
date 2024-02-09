8418194c91a4eac1f37e9f47ff67121cb2e37dc6
Cj Alexander <clauvinsalexander@gmail.com>
2024-02-09 08:51:07 -0500

    Finished

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-const originalBook = new Book()
-const myLibrary = [];
+const originalBook = new Book('The Hobbit', "J.R.R. Tolkien", 295, false, 0);
+const myLibrary = [originalBook];
+let bookId = 1;
 
-function Book(title, author, pages, read) {
+function Book(title, author, pages, read, id) {
     this.title = title;
     this.author = author;
     this.pages = pages;
+    this.id = id;
     if (read) {
         this.read = "read";
     } else {
@@ -20,15 +22,23 @@ Book.prototype.printBook = function() {
     let h4 = document.createElement('h4');
     h4.textContent = this.author;
     let p = document.createElement('p');
-    p.textContent = `${this.pages} pages`
+    p.textContent = `${this.pages} pages`;
+    let deleteButton = document.createElement('button');
+    deleteButton.className = 'delete';
+    deleteButton.id = this.id;
+    deleteButton.textContent = 'Delete Book';
     let read = document.createElement('button');
     read.className = 'read'
     read.textContent = this.read;
+    let id = document.createElement('span');
+    id.textContent = this.id;
 
+    div.appendChild(id);
     div.appendChild(h3);
     div.appendChild(h4);
     div.appendChild(p);
     div.appendChild(read);
+    div.appendChild(deleteButton);
 
     let shelf = document.querySelector('.library-shelf')
     shelf.appendChild(div)
@@ -37,6 +47,16 @@ Book.prototype.printBook = function() {
 function addBookToLibrary(Book){
     myLibrary.push(Book);
     Book.printBook();
+    bookId++;
+}
+
+function removeBookFromLibrary(id) {
+    myLibrary.forEach((book) => {
+        if (book.id == id) {
+            myLibrary.splice(book[id], 1);
+        }
+    })
+    console.log(myLibrary)
 }
 
 
@@ -58,8 +78,9 @@ submitBook.addEventListener('click', (e) => {
     const title = document.querySelector('#title').value
     const author = document.querySelector('#author').value
     const pages = document.querySelector('#pages').value
-    let newBook = new Book(title, author, pages, false)
+    let newBook = new Book(title, author, pages, false, bookId)
     addBookToLibrary(newBook)
+    dialog.close();
 })
 
 document.querySelector('.library-shelf').addEventListener('click', function(e) {
@@ -67,6 +88,16 @@ document.querySelector('.library-shelf').addEventListener('click', function(e) {
         e.target.textContent = e.target.textContent === 'read' ? 'not read yet' : 'read';
     }
 });
+
+document.querySelector('.library-shelf').addEventListener('click', (e) => {
+    if (e.target && e.target.className == 'delete') {
+        const pickedId = parseInt(e.target.id);
+        if (pickedId != 0){
+            removeBookFromLibrary(pickedId);
+            e.target.parentNode.remove();
+        } 
+    }
+})
 
 
 
